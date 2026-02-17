693321a223f3ff28b0902de86b05039f09679ee4
collectifengineering-lang <collectifengineering@gmail.com>
2026-02-17 09:57:24 -0500

    Reorder work entries (by Dinora Negron)

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -197,6 +197,25 @@ export const portfolioData: SeedData = {
             ]
         },
         {
+            id: 37,
+            title: "Tina's Hospital",
+            client: "Happy Doggo",
+            description: "Our team led the MEP/FP engineering design for Tina’s Hospital for Dogs, a new ~40,000-square-foot veterinary facility in Ko Samui, Thailand. This is a meaningful initiative to provide medical care to Thailand’s vulnerable street dogs.  The hospital, designed by Danny Forster & Architecture, features state-of-the-art facilities. ",
+            category: "Cultural",
+            photoCredit: "Moso Studio",
+            architect: "Danny Forster & Architecture",
+            location: "Ko Samui, Thailand",
+            article: "https://www.happydoggo.com/tinas-hospital",
+            images:
+            [
+                "tinas-1.jpg",
+                "tinas-3.jpg",
+                "tinas-4.jpg",
+                "tinas-6.jpg",
+                "tinas-7.jpg",
+            ]
+        },
+        {
             id: 10,
             title: "450 W 31st Street",
             client: "Venmel, LLC",
@@ -383,25 +402,6 @@ export const portfolioData: SeedData = {
                 "nevelhaus-3.jpg",
                 "nevelhaus-4.jpg",
                 "nevelhaus-5.jpg",
-            ]
-        },
-        {
-            id: 37,
-            title: "Tina's Hospital",
-            client: "Happy Doggo",
-            description: "Our team led the MEP/FP engineering design for Tina’s Hospital for Dogs, a new ~40,000-square-foot veterinary facility in Ko Samui, Thailand. This is a meaningful initiative to provide medical care to Thailand’s vulnerable street dogs.  The hospital, designed by Danny Forster & Architecture, features state-of-the-art facilities. ",
-            category: "Cultural",
-            photoCredit: "Moso Studio",
-            architect: "Danny Forster & Architecture",
-            location: "Ko Samui, Thailand",
-            article: "https://www.happydoggo.com/tinas-hospital",
-            images:
-            [
-                "tinas-1.jpg",
-                "tinas-3.jpg",
-                "tinas-4.jpg",
-                "tinas-6.jpg",
-                "tinas-7.jpg",
             ]
         },
         {
